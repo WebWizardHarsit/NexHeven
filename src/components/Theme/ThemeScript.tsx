9f6ca892051c31e578630272d8018f932f0f7e27
harsit-bindhani <harsitbindhani406@gmail.com>
2025-07-27 22:38:55 +0530

    Some Changes

--- src/components/Theme/ThemeScript.tsx
+++ src/components/Theme/ThemeScript.tsx
@@ -1,14 +1,22 @@
-// components/ThemeScript.tsx
+"use client";
+
 export default function ThemeScript() {
-  const code = `
-    try {
-      const theme = localStorage.getItem('theme');
-      if (theme === 'dark') {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-    } catch (e) {}
-  `;
-  return <script dangerouslySetInnerHTML={{ __html: code }} />;
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function () {
+            try {
+              const theme = localStorage.getItem("theme");
+              if (theme === "dark") {
+                document.documentElement.classList.add("dark");
+              } else {
+                document.documentElement.classList.remove("dark");
+              }
+            } catch (e) {}
+          })();
+        `,
+      }}
+    />
+  );
 }
