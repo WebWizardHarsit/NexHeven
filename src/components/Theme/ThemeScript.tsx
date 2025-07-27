f7513614944610ecec557667ec249b8eaa2f4d44
harsit-bindhani <harsitbindhani406@gmail.com>
2025-07-27 22:43:01 +0530

    New

--- src/components/Theme/ThemeScript.tsx
+++ src/components/Theme/ThemeScript.tsx
@@ -6,14 +6,8 @@ export default function ThemeScript() {
       dangerouslySetInnerHTML={{
         __html: `
           (function () {
-            try {
-              const theme = localStorage.getItem("theme");
-              if (theme === "dark") {
-                document.documentElement.classList.add("dark");
-              } else {
-                document.documentElement.classList.remove("dark");
-              }
-            } catch (e) {}
+            // Force light mode
+            document.documentElement.classList.remove('dark');
           })();
         `,
       }}
